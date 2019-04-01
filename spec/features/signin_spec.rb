a7c2ac3dd94370592303291e3800e42b15500de6
Sureka Stoeckigt <sureka.stoeckigt@gmail.com>
2019-04-01 11:10:49 +0100

    l

--- spec/features/signin_spec.rb
+++ spec/features/signin_spec.rb
@@ -1,9 +1,19 @@
 require 'rails_helper'
 
 RSpec.feature "Signin", type: :feature do
-  scenario "Can sign in" do
+
+  scenario "A user can access the signin form" do
     visit "/signin"
     expect(page).to have_content("Username")
-
   end
+
+  scenario "Signs in and goes to next page" do
+    visit '/signup'
+    fill_in 'signup[username]', with: "Foo"
+    fill_in 'signup[password]', with: "Bar"
+    fill_in 'signup[email]', with: "Foo@Bar.com"
+    click_button "Submit"
+    visit '/'
+  end
+  
 end
