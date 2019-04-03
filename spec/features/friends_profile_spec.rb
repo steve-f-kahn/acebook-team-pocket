0b8d5fab1fa3e4c14585b8a7a0e157c09b2d0d75
Amina Jusic <amina_jusic@yahoo.com>
2019-04-03 14:45:32 +0100

    Feature test for friends profile page

---
+++ spec/features/friends_profile_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+RSpec.feature "see friends profile page", type: :feature do
+
+  scenario "A user can see friends sugesstions list" do
+    visit '/signup'
+    fill_in 'signup[username]', with: "Foo"
+    fill_in 'signup[password]', with: "Bar"
+    fill_in 'signup[email]', with: "Foo@Bar.com"
+    click_button "Submit"
+    visit '/signin/new'
+    fill_in 'signin[username]', with: "Foo"
+    fill_in 'signin[password]', with: "Bar"
+    click_button "Signin"
+    click_button "Friends sugesstions"
+    expect(page).to have_content("Friend1")
+  end
+end
