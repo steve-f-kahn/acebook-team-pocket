fe68d420d9a13cf7f02ca22993d0a7f7b675a99f
Steve Kahn <steve@kahn.org.uk>
2019-04-04 12:35:45 +0100

    Friends are shown on friend page, need to add add friend button next

--- app/controllers/friends_controller.rb
+++ app/controllers/friends_controller.rb
@@ -2,4 +2,16 @@ class FriendsController < ApplicationController
   def new
   end
 
+  def index
+    @friends = [] #Friends of profile owner
+    friend_ids = Friend.where("receiver_id = #{params[:id]}", "sender_id = #{params[:id]}")
+    friend_ids.each do |friend|
+      if friend.receiver_id == params[:id].to_i
+        @friends << user = Signup.find_by(id: friend.sender_id)
+      elsif friend.sender_id == params[:id]
+        @friends << user = Signup.find_by(id: friend.receiver_id)
+      end
+    end
+  end
+
 end
